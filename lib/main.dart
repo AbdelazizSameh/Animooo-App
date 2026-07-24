41e72d411aa0c77abf4e44bab1ef543ac5f2e3e0
Abdelaziz Sameh <abdelazizsameh364@gmail.com>
2026-07-24 18:25:12 +0300

    add project assets, colors, styles and font family

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
+import 'animooo_app.dart';
 
 void main() {
   runApp(const AnimoooApp());
-}
-
-class AnimoooApp extends StatelessWidget {
-  const AnimoooApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Animooo App',
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
 }
