5d7ab7b0542beb3c9627f84eb320ec858a1f2c57
Abdelaziz Sameh <abdelazizsameh364@gmail.com>
2026-07-25 01:18:02 +0300

    add go router package and make home screen

---
+++ lib/core/constants/app_routes.dart
@@ -0,0 +1,7 @@
+abstract class AppRoutes {
+  static const loginRoute = '/';
+  static const signupRoute = '/signupRoute';
+  static const forgetPasswordRoute = '/forgetPasswordRoute';
+  static const createNewPasswordRoute = '/createNewPasswordRoute';
+  static const otpVerficationRoute = '/otpVerficationRoute';
+}
---
+++ lib/core/utils/app_router.dart
@@ -0,0 +1,15 @@
+import 'package:go_router/go_router.dart';
+
+import '../../features/auth/presentation/views/login_view.dart';
+import '../constants/app_routes.dart';
+
+abstract class AppRouter {
+  static GoRouter routerConfig = GoRouter(
+    routes: [
+      GoRoute(
+        path: AppRoutes.loginRoute,
+        builder: (context, state) => const LoginView(),
+      ),
+    ],
+  );
+}
